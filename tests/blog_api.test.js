293291e4008d3b613725ed562c81f2c1173b8077
haido86 <thithuhai.do@gmail.com>
2022-02-17 22:28:14 +0200

    4.8: Blog list tests, step1

---
+++ tests/blog_api.test.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
+
+const api = supertest(app);
+const Blog = require("../models/blog");
+
+// test("blogs are returned as json", async () => {
+//   await api
+//     .get("/api/blogs")
+//     .expect(200)
+//     .expect("Content-Type", /application\/json/);
+// }, 100000);
+
+const initialBlogs = [
+  {
+    title: "Going to cinema",
+    author: "hag",
+    url: "String",
+    likes: 72,
+    id: "620c0acac6c7ea818379f9bf",
+  },
+  {
+    title: "Going ",
+    author: "hhgk",
+    url: "String",
+    likes: 92,
+    id: "620c1d58d95045367687a2ff",
+  },
+];
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  let blogObject = new Blog(initialBlogs[0]);
+  await blogObject.save();
+  blogObject = new Blog(initialBlogs[1]);
+  await blogObject.save();
+});
+
+test("there are 2 blogs", async () => {
+  const response = await api.get("/api/blogs");
+  expect(response.body).toHaveLength(2);
+}, 100000);
+
+afterAll(() => {
+  mongoose.connection.close();
+});
