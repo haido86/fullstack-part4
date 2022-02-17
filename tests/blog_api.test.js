6f69b24709becf9a7675fee74813300a4eadab05
haido86 <thithuhai.do@gmail.com>
2022-02-17 22:28:44 +0200

    4.8: Blog list tests, step1

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -5,12 +5,12 @@ const app = require("../app");
 const api = supertest(app);
 const Blog = require("../models/blog");
 
-// test("blogs are returned as json", async () => {
-//   await api
-//     .get("/api/blogs")
-//     .expect(200)
-//     .expect("Content-Type", /application\/json/);
-// }, 100000);
+test("blogs are returned as json", async () => {
+  await api
+    .get("/api/blogs")
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+}, 100000);
 
 const initialBlogs = [
   {
