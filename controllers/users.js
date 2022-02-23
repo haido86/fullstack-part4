bcf4486677471dae324f3160c48cbfac83a70df1
haido86 <thithuhai.do@gmail.com>
2022-02-23 11:33:57 +0200

    4.15: bloglist expansion, step3

---
+++ controllers/users.js
@@ -0,0 +1,29 @@
+const bcrypt = require("bcryptjs");
+const usersRouter = require("express").Router();
+const User = require("../models/user");
+
+usersRouter.get("/", async (request, response) => {
+  const users = await User.find({});
+  response.json(users);
+});
+
+usersRouter.post("/", async (request, response) => {
+  const { username, name, password } = request.body;
+  console.log("request.body", request.body);
+
+  const salt = await bcrypt.genSaltSync(10);
+
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  const user = new User({
+    username,
+    name,
+    passwordHash,
+  });
+
+  const savedUser = await user.save();
+
+  response.status(201).json(savedUser);
+});
+
+module.exports = usersRouter;
