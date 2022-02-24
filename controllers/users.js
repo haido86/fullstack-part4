1894d97f3f8a5115f159e20c48a1c665f1ef72d9
haido86 <thithuhai.do@gmail.com>
2022-02-24 14:10:25 +0200

    4.17: bloglist expansion, step5

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,7 @@ const usersRouter = require("express").Router();
 const User = require("../models/user");
 
 usersRouter.get("/", async (request, response) => {
-  const users = await User.find({});
+  const users = await User.find({}).populate("blogs");
   response.json(users);
 });
 
@@ -42,6 +42,16 @@ usersRouter.post("/", async (request, response) => {
   const savedUser = await user.save();
 
   response.status(201).json(savedUser);
+});
+
+usersRouter.delete("/:id", async (request, response, next) => {
+  try {
+    console.log("params.id", request.params.id);
+    await User.findByIdAndRemove(request.params.id);
+    response.status(204).end();
+  } catch (exception) {
+    next(exception);
+  }
 });
 
 module.exports = usersRouter;
